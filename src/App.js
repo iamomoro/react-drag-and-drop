5629fd2cde880bb74d38ee91f2d54c2d377518fb
Elvis Omoro <iamomoroit@gmail.com>
2022-11-02 10:07:21 +0300

    set-main-apge board-cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import React from 'react';
+import { Board, Card } from './components';
 
 function App() {
   return (
     <div className="App">
       <main className="flexbox">
-        Here will be our boards
+       <Board id='board-1' className='board'>
+        <Card id='card-1' className='card' draggable='true'>
+          <p>Card One</p>
+        </Card>
+       </Board>
+
+       <Board id='board-2' className='board'>
+        <Card id='card-2' className='card' draggable='true'>
+          <p>Card Two</p>
+        </Card>
+        <Card id='card-3' className='card' draggable='flase'>
+          <p>Card Three</p>
+        </Card>
+       </Board>
       </main>
     </div>
   );
